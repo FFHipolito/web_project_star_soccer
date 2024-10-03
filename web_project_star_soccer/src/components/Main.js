6c376c4700745fa3b8aa9ef6ccc34a9bd4041266
FernandoHipolito <fernandofhipolito@gmail.com>
2024-10-03 16:05:58 -0300

    create main component

--- web_project_star_soccer/src/components/Main.js
+++ web_project_star_soccer/src/components/Main.js
@@ -1,5 +1,42 @@
 function Main() {
-  return <p>HOME PAGE</p>;
+  // TODO remove hardcode below after be integration
+  const hasNextMatch = true;
+  const isUserAdmin = false;
+  const matchDate = "20/09/2024";
+  const matchTime = "20:00";
+
+  const title = hasNextMatch ? "Next Match" : "There is no match created yet.";
+  return (
+    <div className="main">
+      <h2 className="main__title">{title}</h2>
+      {!hasNextMatch && isUserAdmin && (
+        <button type="button" className="main__button">
+          Create a new match
+        </button>
+      )}
+      {hasNextMatch && (
+        <>
+          <div className="main__card">
+            <div className="main__card_container">
+              <p className="main__card_date">{matchDate}</p>
+              <span className="main__card_time">time: {matchTime}h</span>
+            </div>
+          </div>
+          <button type="button" className="main__button">
+            I gonna play!
+          </button>
+          {isUserAdmin && (
+            <button type="button" className="main__button">
+              See players
+            </button>
+          )}
+        </>
+      )}
+      <button type="button" className="main__button">
+        Edit my profile
+      </button>
+    </div>
+  );
 }
 
 export default Main;

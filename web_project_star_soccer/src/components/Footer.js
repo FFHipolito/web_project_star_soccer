97fc901be4c9ef092a8429028ce3ce97dcf9c7ba
FFHipolito <148506148+FFHipolito@users.noreply.github.com>
2024-10-12 20:46:37 -0300

    Delete web_project_star_soccer directory

--- web_project_star_soccer/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__paragraph">© 2024 Super Soccer App.</p>
-    </footer>
-  );
-}
-
-export default Footer;
